2dbf874aff481df6a5ce16cf1c217e5e117df65b
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-12-19 16:47:59 +0700

    add case 2

--- Exercise5/Program.cs
+++ Exercise5/Program.cs
@@ -86,6 +86,10 @@ namespace Exercise5
                         string num = Convert.ToString(Console.ReadLine());
                         q.insert(num);
                         break;
+                    case '2':
+                        q.delete();
+                        Console.WriteLine("item delete");
+                        break;
                 }
             }
         }
