e8188c76ba83d4a343b740023d22e44c5720b591
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-12-19 16:03:55 +0700

    add class node

--- Exercise5/Program.cs
+++ Exercise5/Program.cs
@@ -1,1 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    public class QueueLinkedList
+    {
+        class Node
+        {
+            public int TF;
+            public Node next;
+            public Node(int tf)
+            {
+                TF = tf;
+            }
+        }
+    }
+}
